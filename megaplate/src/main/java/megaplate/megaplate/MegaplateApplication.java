e799afa07ae3e707a3c80ff4c95d6b9b64370e2e
miguelsantos1 <miguelsantos6700@gmail.com>
2025-02-22 11:58:59 -0300

    feat/estrura padrao feita

---
+++ megaplate/src/main/java/megaplate/megaplate/MegaplateApplication.java
@@ -0,0 +1,13 @@
+package megaplate.megaplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaplateApplication.class, args);
+	}
+
+}
---
+++ megaplate/src/test/java/megaplate/megaplate/MegaplateApplicationTests.java
@@ -0,0 +1,13 @@
+package megaplate.megaplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
