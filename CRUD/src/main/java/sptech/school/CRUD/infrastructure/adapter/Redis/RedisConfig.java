a58e36551ed6bd188af12b06dd0f45ce75c48daa
GabrielNogueira33 <gabriel.ncarvalho@sptech.school>
2025-12-05 16:37:11 +0000

    cloud: preparando ambiente para containers funcionais

--- CRUD/src/main/java/sptech/school/CRUD/infrastructure/adapter/Redis/RedisConfig.java
+++ CRUD/src/main/java/sptech/school/CRUD/infrastructure/adapter/Redis/RedisConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
