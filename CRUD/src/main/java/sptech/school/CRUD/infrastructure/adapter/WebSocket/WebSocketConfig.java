2707d33212d6eac3f6e975b3a82ee6de16807caf
GabrielNogueira33 <gabriel.ncarvalho@sptech.school>
2025-12-07 22:51:56 -0300

    fix: websocket funcional remotamente

--- CRUD/src/main/java/sptech/school/CRUD/infrastructure/adapter/WebSocket/WebSocketConfig.java
+++ CRUD/src/main/java/sptech/school/CRUD/infrastructure/adapter/WebSocket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package sptech.school.CRUD.infrastructure.adapter.WebSocket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-notifications") // endpoint puro
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins(allowedOrigins)
                 .withSockJS();
     }
 
